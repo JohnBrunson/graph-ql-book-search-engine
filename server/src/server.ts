415d07b522e74697976a2f87b9fbfd8f847ed395
JohnBrunson <JohnFBrunson@gmail.com>
2025-03-22 14:47:13 -0700

    Fixing server path issue take two

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,11 +26,15 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 //import path from 'path';
 import path from 'node:path';
+import { fileURLToPath } from 'url';
 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 import { jwt } from './utils/auth.js';
 
+// Manually define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -71,7 +75,8 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/dist')));
+    //app.use(express.static(path.join(__dirname, '../../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client')));
 
     app.get('*', (_req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
